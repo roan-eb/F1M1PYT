6485f1bbb223dd8129084356ec9da4a00c7a9841
53v <roan.esteban2@gmail.com>
2021-10-26 10:15:59 +0200

    first commit

---
+++ L2 - Interesting Tell Me More/tell_me_more.py
@@ -0,0 +1,18 @@
+import time 
+
+antwoord1 = input("Wat is je naam? ")
+print ('Hallo '+ antwoord1 +', leuk om je te ontmoeten. ')
+
+time.sleep(2.0)
+
+antwoord2 = input("Hoe oud ben je? ")
+print ('Je bent dus '+ antwoord2 +' jaar oud.' )
+
+time.sleep(2.0)
+
+antwoord3 = input("Waar zit je op school? ")
+print ('Het '+ antwoord3 +' klinkt als een leuke school. ')
+
+time.sleep(2.0)
+
+print ('Hallo '+ antwoord1 +', je bent dus '+ antwoord2 +' en zit op het '+ antwoord3 +'? Fijn om je beter te leren kennen!')
---
+++ L3 - Daily Choices/dailtchoices.py
@@ -0,0 +1,22 @@
+
+
+print("You woke up late and missed the bus, do you start walking or wait for the next bus?")
+choice = input()
+if choice == 'start walking':
+    print("You started walking and realized you will be too late at work, you start running as fast as possible.")
+elif choice == 'next bus':
+    print("If you hurry you will be at work just in time.")
+else:
+    print(choice, " wasn't a valid choice")
+
+print("You arrived at work just in time. Your boss is mad because you had to be at work early to help a customer. Do you make an excuse or explain what happenend?")
+choice = input()
+if choice == 'make an excuse.':
+    print("The bus wasn't driving I had to walk I couldn't do anything against it. ")
+elif choice == 'Explain what happenend':
+    print("I woke up late so I missed the bus, I'm sorry. ")
+else:
+    print(choice, " wasn't a valid choice")
+
+    
+    
